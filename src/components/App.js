1cf256805b2bbc8da171139f04a6f40f1008c4c1
Adilet312 <adiletm@hotmail.com>
2020-08-11 23:32:01 -0700

    Add starter.

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './css/app.css';
+
+
+ const App = () =>{
+   return(
+<React.Fragment>
+   <header className = "header-nav"></header>
+   <main className ="container">
+
+   </main>
+</React.Fragment>
+   )
+ }
+export default App;
