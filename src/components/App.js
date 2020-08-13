5f344be56df2084d2b95e3c63e17f92d463c0c90
Adilet312 <adiletm@hotmail.com>
2020-08-12 19:34:49 -0700

    Add menu when it clicked.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import './css/app.css';
+import React, { useState } from 'react';
+import './css/App.css';
 
 
  const App = () =>{
+   const[getMenu,setMenu] = useState("site")
    return(
-<React.Fragment>
-   <header className = "header-nav"></header>
-   <main className ="container">
-
-   </main>
-</React.Fragment>
+      <React.Fragment>
+        <div className = {getMenu}>
+          <header></header>
+          <section className = "main-nav">
+              <button className = "trigger" onClick={()=>{
+                  getMenu === "site" ? setMenu("site reveal") : setMenu("site")}}>Menu</button>
+              <nav>
+                 <ul>
+                     <li><a href="#">Javascript</a></li>
+                     <li><a href="#">Python</a></li>
+                     <li><a href="#">Java</a></li>
+                     <li><a href="#">C#</a></li>
+                 </ul>
+               </nav>
+          </section>
+          <main></main>
+          <aside></aside>
+          <footer></footer>
+         </div>
+      </React.Fragment>
    )
  }
 export default App;
